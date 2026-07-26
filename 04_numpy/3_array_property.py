e40c7f15a12ca5c41cd44fd8cbc6e55823584f47
Tanish Mhatre <mhatretanish417@gmail.com>
2026-07-26 21:02:59 +0530

    Add NumPy practice exercises and examples

--- 04_numpy/3_array_property.py
+++ 04_numpy/3_array_property.py
@@ -42,3 +42,15 @@ int_arr=arr.astype(int)
 print(int_arr)
 print(int_arr.dtype)
 
+
+
+
+
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+newarr = arr.reshape(4, 3)
+
+print(newarr)
