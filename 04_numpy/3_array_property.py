f2f1c6216e76c1f1901a5a415d17d80ccb9351cc
Tanish Mhatre <mhatretanish417@gmail.com>
2026-07-26 20:58:49 +0530

    Add NumPy practice exercises and examples

--- 04_numpy/3_array_property.py
+++ 04_numpy/3_array_property.py
@@ -15,7 +15,6 @@ ar_2d=np.array([[1,2,3],
 print(ar_2d.size)
 
 
-#--------------------------------------
 
 #ndim
 
@@ -25,6 +24,21 @@ arr_1d=np.array([1,2,3])
 arr_2d=np.array([[1,2,3],[4,5,6]])
 arr_3d=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
 
-print("array dimession",arr_1d.ndim)
-print("array dimession",arr_2d.ndim)
-print("array dimession",arr_3d.ndim)
+print("array dimession :",arr_1d.ndim)
+print("array dimession :",arr_2d.ndim)
+print("array dimession :",arr_3d.ndim)
+
+#data type in array 
+
+import numpy as np
+arr=np.array([1,2,3,4])
+print(arr.dtype)
+
+#astype 
+import numpy as np
+arr=np.array([1.5,2,3,4,5,6,7,8.2,9])
+int_arr=arr.astype(int)
+
+print(int_arr)
+print(int_arr.dtype)
+
