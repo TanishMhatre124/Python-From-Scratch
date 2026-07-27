e7902204bb131fefa03267df6df12262fa1606f1
Tanish Mhatre <mhatretanish417@gmail.com>
2026-07-27 23:39:18 +0530

    Complete NumPy vectorization with practical examples

---
+++ 04_numpy/10_vectorization_array.py
@@ -0,0 +1,23 @@
+#vectorization 
+
+list1=[1,2,3]
+list2=[4,5,6]
+
+result=[x + y for x,y in zip(list1,list2)]
+print(result)
+
+#===============================================
+import numpy as np
+
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+result=arr1 + arr2
+print(result)
+
+#==============================
+import numpy as np
+
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+result=arr1 * arr2
+print(result)
