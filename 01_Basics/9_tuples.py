126efeab3b4783652ac626fecd1450bbb41542d8
Tanish Mhatre <mhatretanish417@gmail.com>
2026-08-05 19:07:14 +0530

    Complete Tuples  with concepts

---
+++ 01_Basics/9_tuples.py
@@ -0,0 +1,17 @@
+#tuples 
+#immutable once created cannot be changed 
+
+#creating   tuple 
+tup=(0,1,2,3)
+print(type(tup))
+print(tup[0])
+
+print(tup[1:3])
+
+#tuple method 
+
+tup=(0,1,2,3,4,5,6,7,8,9,2,2)
+print(tup.index(2))
+print(tup.count(2))
+
+
